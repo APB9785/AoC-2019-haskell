a286106d83a9fb9c991996948b9d50d135f8e523
Andy <74077809+APB9785@users.noreply.github.com>
2020-11-17 16:14:49 -0600

    Update day_10.hs

--- day_10.hs
+++ day_10.hs
@@ -1,6 +1,6 @@
 import Data.Function
 import Data.Ord (comparing)
-import Data.List (maximumBy)
+import Data.List (maximumBy, delete, sort, sortOn, (!!))
 
 main = do
     contents <- readFile "day10input.txt"
@@ -16,7 +16,14 @@ main = do
 
     putStr "\nPart 2:  "
 
-    destroy asteroidList coords
+    -- The base location is no longer a target so we can delete it.
+    destroy (delete coords asteroidList) coords
+      & take200th
+      & formatResult
+      & print
+
+    putStr "\n"
+
 
 -- Loops over the input with chartSingleRow to create a list containing
 -- the coordinates of each asteroid.
@@ -51,13 +58,39 @@ checkSights astList base = go astList base []
 -- Returns a list of each asteroid destroyed from the base location in 
 -- clockwise order, with blocked asteroids surviving until a subsequent
 -- rotation of the beam.
-destroy :: [(Int, Int)] -> (Int, Int) -> [(Int, Int)]
+destroy :: [(Int, Int)] -> (Int, Int) -> [(Float, Int, Int)]
 destroy astList base =
-    sortOn (findAngle base) astList
+    let start = sort $ map (findAngle base) astList in
+    loop start [] []
+    where
+      loop [] []   done = reverse done
+      loop [] hold done = loop hold [] done
+      loop ((a, ax, ay):(b, bx, by):rest) hold done
+        | a == b = 
+          let (same, others) = break (\(i, j, k) -> i /= a) ((a, ax, ay):(b, bx, by):rest)
+              (closest:toHold) = sortOn (distanceFrom base) same in
+          loop others (hold ++ toHold) (closest:done)
+        | otherwise =
+          loop ((b, bx, by):rest) hold ((a, ax, ay):done)
+      loop [x] hold done = loop [] hold (x:done)
+
+distanceFrom :: (Int, Int) -> (Float, Int, Int) -> Int
+distanceFrom (bx, by) (t, x, y) = (abs (bx - x)) + (abs (by - y))
 
 -- Calculates the angle at which the beam must fire from the base to hit
--- the given asteroid.
-findAngle base ast = atan $ compareCoords
+-- the given asteroid. Output is a tuple (t, x, y) where t is the angle in radians.
+findAngle :: (Int, Int) -> (Int, Int) -> (Float, Int, Int)
+findAngle base (a, b) =
+    let (x, y) = compareCoords base (a, b) in
+    case (fromIntegral x, fromIntegral y) of
+      (x', y') | (x == 0) && (y > 0) -> (0, a, b)
+               | (y == 0) && (x > 0) -> (pi / 2, a, b)
+               | (x == 0) && (y < 0) -> (pi, a, b)
+               | (y == 0) && (x < 0) -> ((3 * pi) / 2, a, b)
+               | (x > 0) && (y > 0)  -> (atan (x' / y'), a, b)
+               | (x > 0) && (y < 0)  -> ((pi / 2) + (atan (-y' / x')), a, b)
+               | (x < 0) && (y < 0)  -> (pi + (atan (x' / y')), a, b)
+               | (x < 0) && (y > 0)  -> (((3 * pi) / 2) + (atan (y' / (-x'))), a, b)
     
 -- Simple dx/dy calculation.  We use the opposite of dy because the map
 -- was scanned from the top down.
@@ -76,6 +109,12 @@ reduceRatio (0, y)
 reduceRatio (x, y) =
     let i = gcd x y in
     ((x `div` i), (y `div` i))
+
+take200th :: [a] -> a
+take200th xs = xs !! 199
+
+formatResult :: (Float, Int, Int) -> Int
+formatResult (t, x, y) = x * 100 + y
 
 formatHeader = "\n***********************\nADVENT OF CODE - DAY 10" ++
     "\n***********************\n\nPart 1:  "
